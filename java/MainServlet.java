21f598556ce16667c5b79f7f8d30260d1124c7c4
OpenShift guest <shinsuke.mat@gmail.com>
2013-09-02 03:12:58 -0400

    simplify server side code

--- java/MainServlet.java
+++ java/MainServlet.java
@@ -4,7 +4,6 @@ import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.logging.Logger;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,100 +15,41 @@ import org.apache.catalina.websocket.WsOutbound;
 
 
 @WebServlet(value = { "/send" })
-public class MainServlet extends WebSocketServlet {
-
-	/**
-	 * serialVersionUID
-	 */
+public class CMainServlet extends WebSocketServlet {
 	private static final long serialVersionUID = 6946416208261279049L;
-
-	/**
-	 * スレッドセーフなEchoInboundのSet
-	 */
 	private static final Set<ChatMessageInbound> inbounds = new CopyOnWriteArraySet<ChatMessageInbound>();
 
-	private static Logger logger;
-
-	public MainServlet() {
-		logger = Logger.getLogger(getClass().getName());
-	}
-
-
-	/**
-	 * 新規クライアント接続時の処理
-	 */
 	@Override
 	protected StreamInbound createWebSocketInbound(String subProtocol, HttpServletRequest request) {
-		return new ChatMessageInbound(this);
+		return new ChatMessageInbound();
 	}
 
-
-	/**
-	 * 送受信をお行うためのオブジェクト
-	 */
 	class ChatMessageInbound extends MessageInbound {
-		private WebSocketServlet mainServlet;
-
-		public ChatMessageInbound(MainServlet mainServlet) {
-			this.mainServlet = mainServlet;
-			log("connect", "");
-		}
-
-		/**
-		 * 接続時の処理
-		 */
 		@Override
 		protected void onOpen(WsOutbound outbound) {
-			log("open", outbound.toString());
-
-			// EchoServletの持つsocketリストに自分自身を保存する
 			inbounds.add(this);
 		}
 
-		/**
-		 * 切断時の処理
-		 */
 		@Override
 		protected void onClose(int status) {
-			log("close", String.valueOf(status));
 			inbounds.remove(this);
 		}
 
-		/**
-		 * バイナリメッセージ受信時の処理
-		 */
 		@Override
-		protected void onBinaryMessage(ByteBuffer message) throws IOException {
-			// 何もしない
-		}
+		protected void onBinaryMessage(ByteBuffer message) throws IOException {}
 
-		/**
-		 * テキストメッセージ受信時の処理
-		 */
 		@Override
 		protected void onTextMessage(CharBuffer buffer) throws IOException {
-			log("send", buffer.toString());
 			sendMessages(buffer);
 		}
 
-		private void log(String type, String message) {
-			logger.info("[" + type + "] (" + mainServlet.getServletContext().getContextPath() + ") " + message);
-		}
-
-		/**
-		 * 全クライアントにメッセージを送信する
-		 * @param msgObject
-		 * @throws IOException
-		 */
 		private void sendMessages(CharBuffer buffer) throws IOException{
 			try {
 				for (MessageInbound socket : inbounds) {
 					socket.getWsOutbound().writeTextMessage(buffer);
 					buffer.position(0);
 				}
-			} catch(IOException e) {
-				e.printStackTrace();
-			}
+			} catch(IOException e) {}
 		}
 	}
 }
